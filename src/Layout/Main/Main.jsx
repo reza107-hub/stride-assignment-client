03e4622c2728a19525718b4a534170f656f31fb9
reza107-hub <rashidaminur104@gmail.com>
2024-12-20 09:51:44 +0600

    first commit

---
+++ src/Layout/Main/Main.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "../../components/NavBar/NavBar";
+
+const Main = () => {
+  return (
+    <div>
+      <NavBar/>
+      <Outlet/>
+    </div>
+  )
+};
+
+export default Main
